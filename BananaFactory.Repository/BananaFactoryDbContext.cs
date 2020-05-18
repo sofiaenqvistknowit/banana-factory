9a907765c198b4382a5850a875cbffea9187e811
Pontus Wadsjö <pontus.wadsjo@knowit.se>
2020-05-18 10:21:38 +0200

    Uppdaterat till .Net 5

---
+++ BananaFactory.Repository/BananaFactoryDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BananaFactory.Repository
+{
+    public class BananaFactoryDbContext : DbContext
+    {
+    }
+}
--- BananaFactory.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BananaFactory.Repository
-{
-    public class Class1
-    {
-    }
-}
